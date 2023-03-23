847805b53ae295421f23938234bae0ad1433f42d
animemuically6@gamil.com <animemuically6@gamil.com>
2023-03-23 17:25:34 +0530

    web-side

---
+++ main.js
@@ -0,0 +1,20 @@
+console.log('Hello World!'); 
+alert("आप सभी को मेरा नमस्ते");
+
+   var tablink = 
+  document.getElementsByClassName("tab-link"); 
+    //var tabcantainer = 
+    document.getElementsByClassName("tab-cantainer");
+    
+    
+    //function opentab(tabname){
+      //for(tablink of tablinks)
+     // { tablink.Classlist.remove("active-link");}
+      
+     // for(tabcantainer of tabcantainers)
+      //{tabcantainer.Classlist.remove("active-tab");}
+    }
+  
+  
+    
+  
